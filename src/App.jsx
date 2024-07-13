02a2a4fd9c6994d1a59d74f95d3ee0046ce48431
Karan Vasupalli <vkaran14210@gmail.com>
2024-07-13 18:55:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+import data from './data.js'
+
+function App() {
+  const [number, setNumber] = useState(); 
+  const [para, setpara] = useState([]);
+
+
+  const generate = () => {
+    if(number <= 0){
+      alert("Please enter value greater than 0 ")
+    }
+    else{
+      if(number > 10){
+        alert("Please enter value less than 11")
+      }
+      else{
+        setpara(data.slice(0, number))
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="bg-red-200 h-max">
+        <h1 className=" text-center mb-14 text-red-500 text-7xl">
+          Paragraph Generator
+        </h1>
+        <div>
+          <div className=" flex gap-4 justify-center items-center mb-7" >
+            <h2 className=" text-3xl">Paragraphs :</h2>
+            <input
+              type="number"
+              name=""
+              id=""
+              className=" outline-gray-800 border  border-slate-900 px-2 w-32 rounded-sm"
+              value={number}
+              onChange={(e) => {
+                setNumber(e.target.value);
+              }}
+            />
+            <button onClick={generate} className=" px-4 py-2 bg-red-500  rounded-md text-white">
+              GENERATE
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2 ">
+        {
+          para.map((item, index) => {
+            return <p className="w-[650px] text-xl border border-black p-3 " key={index}>{item}</p>;
+          })
+        }
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = [
+    `React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.`,
+    
+    `Whether you work on your own or with thousands of other developers, using React feels the same. It is designed to let you seamlessly combine components written by independent people, teams, and organizations.`,
+
+    `This markup syntax is called JSX. It is a JavaScript syntax extension popularized by React. Putting JSX markup close to related rendering logic makes React components easy to create, maintain, and delete.`,
+
+    `React components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data.`,
+
+    `React is a library. It lets you put components together, but it doesn’t prescribe how to do routing and data fetching. To build an entire app with React, we recommend a full-stack React framework like Next.js or Remix.`,
+
+    `People love web and native apps for different reasons. React lets you build both web apps and native apps using the same skills. It leans upon each platform’s unique strengths to let your interfaces feel just right on every platform.`,
+
+    `With React, you can be a web and a native developer. Your team can ship to many platforms without sacrificing the user experience. Your organization can bridge the platform silos, and form teams that own entire features end-to-end.`,
+
+    `React approaches changes with care. Every React commit is tested on business-critical surfaces with over a billion users. Over 100,000 React components at Meta help validate every migration strategy.`,
+
+    `The React team is always researching how to improve React. Some research takes years to pay off. React has a high bar for taking a research idea into production. Only proven approaches become a part of React.`,
+
+    `This blog is the official source for the updates from the React team. Anything important, including release notes or deprecation notices, will be posted here first. You can also follow the @reactjs account on Twitter, but you won’t miss anything essential if you only read this blog.`
+];
+
+export default data;
